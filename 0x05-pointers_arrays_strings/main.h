dd1e2e25de99c223ea2f8208e209219b72dcabc6
Blankerspace <praiseaderonke@gmail.com>
2022-04-20 09:25:50 -0700

    Reset to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,9 +5,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-void puts_half(shar *str);
+void puts_half(char *str);
 void print_array(int *a, int n);
 char*_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
