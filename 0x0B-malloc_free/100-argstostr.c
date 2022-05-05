cb792e65e36e9c85c1eef97dae5bc1356e8204da
Blankerspace <praiseaderonke@gmail.com>
2022-05-05 14:32:21 -0700

    Hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * argtostr - concatenates all arguments of the program.
+ * argstostr - concatenates all arguments of the program.
  * @ac: argument count.
  * @av: pointer to array of size ac.
  * Return: NULL if ac == 0 or av == null, Pointer to new string.
