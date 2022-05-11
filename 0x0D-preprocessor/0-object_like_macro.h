a456c07e934f3172bd60597ea8ab9cc759e24008
Blankerspace <praiseaderonke@gmail.com>
2022-05-11 13:04:18 -0700

    Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,22 +1,7 @@
 #include <stdio.h>
+
 #define SIZE 1024
 #ifndef SIZE 1024
 
 #endif
 
-
-/**
- * main - check the code
- *
- * Return - Always 0.
- */
-
-int main(void)
-{
-	int s;
-
-	s = 98 + SIZE;
-	printf("%d\n", s);
-
-	return (0);
-}
---
+++ 0x0D-preprocessor/blueface.h
@@ -0,0 +1,6 @@
+#ifndef BLUE_H
+#define BLUE_H
+
+int _putchar(char c);
+
+#endif /*GROWING IN SPACE*/
