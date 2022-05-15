e8daf64f53e9e8034388d0fc6b36110973a0dae3
Blankerspace <praiseaderonke@gmail.com>
2022-05-15 08:30:01 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
 #ifndef _CALC_H_
 #define _CALC_H_
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -8,7 +9,7 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 /**
- * struct op - struct op
+ * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
@@ -19,4 +20,5 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * get_op_func - compares function with pointer and returns the equal
+ * get_op_func - Compares function with pointer and returns the equal
  * @s: points a char
  * Return: Returns the function and the operator when equal
  **/
@@ -12,7 +12,7 @@ int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-		{"_", op_sub},
+		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
