9f9ebdfa5b9d46fd53820938000ecb9d63b66b60
Blankerspace <praiseaderonke@gmail.com>
2022-05-16 03:49:44 -0700

    Variadic Functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * main - Entry point 
+ *
+ * @sum: sums all of its parameters.
+ * 
+ * Return: Always 0.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+{
+	int n = 0;
+
+	printf("%d\n", sum);
+
+	return (0)
+}
+
